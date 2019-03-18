043681402b2452978e50723282b7619a5cccfc3f
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-03-18 09:26:59 +0800

    add webpack demo

---
+++ react-webpack-babel-demo/src/index.js
@@ -0,0 +1,4 @@
+var text = require('./hello');
+console.log(text);
+require('./index.css');
+document.body.appendChild(document.createElement('div'));
---
+++ react-webpack-babel-demo/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'use plugin'
+    })
+  ]
+};
