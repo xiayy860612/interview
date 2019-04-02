6f08918161c8e0ce4618f878f45433aa5f0513f7
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-04-02 17:33:46 +0800

    add basic files for demo

---
+++ spring-boot2-java11-maven/src/main/java/com/s2u2m/example/sb2java11/SpringBoot2Java11Application.java
@@ -0,0 +1,13 @@
+package com.s2u2m.example.sb2java11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBoot2Java11Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2Java11Application.class, args);
+    }
+}
